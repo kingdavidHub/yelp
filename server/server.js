e448fa22b88084e88650198bf994a92b14e835fb
Kingdavid <kingigbayilola@gmail.com>
2024-06-28 23:14:10 +0100

    first commit

---
+++ server/server.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const express = require('express');
+
+const app = express();
+
+
+const PORT = process.env.PORT || 5050;
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
