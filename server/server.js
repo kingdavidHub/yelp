14a2b8e9a8299c9635775d9a4e66783448006cb9
Kingdavid <kingigbayilola@gmail.com>
2024-06-29 23:41:17 +0100

    template for yelp

--- server/server.js
+++ server/server.js
@@ -1,12 +1,27 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 
 const app = express();
 
-
 const PORT = process.env.PORT || 5050;
 
+app.get("/test", (req, res) => {
+  return res.status(200).json({
+    test: "Script test",
+  });
+});
 
+app.get("*", (req, res, next) => {
+  res.status(404);
+  next(new Error("Not found"));
+});
+
+app.use((err, req, res, next) => {
+  res.status(res.statusCode).json({
+    status: "error",
+    message: err.message,
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
